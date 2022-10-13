302ca36742446bc62513f24aeacdfcbbd277a987
VictordaSilvaf <victordasilvafernandes@gmail.com>
2022-10-13 17:56:07 -0300

    Menu

--- resources/views/livewire/components/menu.blade.php
+++ resources/views/livewire/components/menu.blade.php
@@ -1,9 +1,19 @@
-<div class="bg-zinc-900 dark:bg-zinc-900 w-full p-2 flex items-center">
-    <div class="text-white ml-4 uppercase">
+<div class="bg-zinc-900 dark:bg-zinc-900 w-full p-2 grid grid-cols-12 text-white">
+    <div class="ml-4 uppercase col-span-1 flex justify-center items-center">
         {{ config('app.name') }}
     </div>
-    <div class="my-2 mr-4 w-full flex justify-end">
 
+    <div class="w-full flex justify-center col-span-10 flex justify-center items-center">
+        <div class="flex gap-8">
+            <x-menu.button route="#">Inicio</x-menu.button>
+            <x-menu.button route="#">Catalogo</x-menu.button>
+            <x-menu.button route="#">Sobre Nós</x-menu.button>
+            <x-menu.button route="#">Dúvidas</x-menu.button>
+            <x-menu.button route="#">Trabalhe Conosco</x-menu.button>
+        </div>
+    </div>
+
+    <div class="my-2 mr-4 w-full flex justify-end">
         @if (Route::has('login'))
             <div class="space-x-4 text-white flex flex-row">
                 @livewire('components.dark-theme-btn')
